0fcd33cb999b76405e38e9e90e4ad5824428a354
deki231 <denchik192010@gmail.com>
2026-05-28 20:10:49 +0300

    hometask

--- total_salary.py
+++ total_salary.py
@@ -1,5 +1,20 @@
 def total_salary(path):
     with open(path, 'r', encoding='utf-8') as file:
-        lines = content.splitlines()
+        lines = file.read().splitlines()
+        total = 0
+        for line in lines:
+            parts = line.split(',')
+            if len(parts) >=2:
+                try:
+                    salary = float(parts[1].strip())
+                    total += salary
+                except ValueError:
+                    continue
+    
+    average = total / len(lines) if lines else 0
+    return total, average            
+            
+
+
 
 total_salary("path.txt")
